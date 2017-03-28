e739d63d8a74ece8676d6effcb72dbd54da56fc0
CoopeKao <kevinkaocooper@gmail.com>
2017-03-28 10:38:21 -0700

    hello

---
+++ csstype/js/csstype.js
@@ -0,0 +1,24 @@
+
+/*
+$(document).ready(function() {
+    	$(".C3").hover(function(){
+    		$(".d3").addClass('gone');
+    	});
+    });
+
+$('body').keydown(function(e){
+	if(e.keyCode == 65;
+	$("")[0].currentTime=0;
+	$("")[0].play();
+}
+
+$('ghostlink').mousedown(function() {
+  var link = $(this).html();
+  $(this).contents().wrap('csstypeface.html');
+});
+*/
+
+$( "first1" ).hover(function() {
+  $( this ).fadeIn( 500 );
+  $( this ).fadeOut( 100 );
+});
---
+++ csstype/js/quote.js
@@ -0,0 +1,30 @@
+
+
+  $(".first1").mousedown(function(){ 
+      
+      $('this').fadeIn('slow');
+  });
+
+/*
+
+    	$(".C3").hover(function(){
+    		$(".d3").addClass('gone');
+    	});
+    });
+
+$('body').keydown(function(e){
+	if(e.keyCode == 65;
+	$("")[0].currentTime=0;
+	$("")[0].play();
+}
+
+$('ghostlink').mousedown(function() {
+  var link = $(this).html();
+  $(this).contents().wrap('csstypeface.html');
+});
+
+
+$( ".first1" ).mousehover(function() {
+  $( this ).removeclass();
+  $( this ).fadeIn( 'slow' );
+*/
